9b73798a757654ab9dbe8191b9e1af90a9dbaf0b
Sabarinath P <98906581+Sabari-nath-p@users.noreply.github.com>
2023-05-30 23:12:31 +0530

    Delete app.js
    test

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.get('/hello_world',(req,res)=>{
-    return res.status(200).send('Hello_World');
-})
-
-exports.app = functions.https.onRequest(app);
