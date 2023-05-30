a628b1d686d547598bb203ad2e5c13dacf85f367
Abijith K A <abijithka02@gmail.com>
2023-05-30 22:34:42 +0530

    Added an hello world restful api

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.get('/hello_world',(req,res)=>{
+    return res.status(200).send('Hello_World');
+})
+
+exports.app = functions.https.onRequest(app);
